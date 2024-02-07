56971dbf0911baa65f9cf9ff55d1dfa37120b084
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-02-07 06:39:08 -0500

    empleados

---
+++ empleados.py
@@ -0,0 +1,16 @@
+nEmpleados = int(input("Ingrese el número de empleados: "))
+contadorSuperior = 0
+contadorInferior = 0
+importe = 0
+
+for i in range(nEmpleados):
+  sueldo = float(input(f"Ingrese el sueldo del empleado {i+1}: "))
+  importe += sueldo
+  if sueldo > 300000 and sueldo < 10000000:
+    contadorInferior += 1
+  elif sueldo > 3000000 and sueldo < 10000000:
+    contadorSuperior += 1
+
+print(f"Empleados con sueldo superior a 3.000.000: {contadorSuperior}")
+print(f"Empleados con sueldo inferior a 3.000.000: {contadorInferior}")
+print(f"El importe total de sueldos es: {importe}")
